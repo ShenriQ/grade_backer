99ea24c7e01ca45564cf118275a49a25ad3562b1
dev <dev@dev.com>
2021-09-26 04:38:33 +0200

    android app name fix

--- android/app/src/debug/java/com/myreactapp/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/myreactapp/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.grade_backer_v01;
+package com.grade_backer_v1;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
